d33df004ff224627e26c695b2cb5029fe17b18ea
Victor Spolaor <victorspolaor@protonmail.com>
2020-12-21 23:20:41 -0300

    Fixing disclaimer and help action

--- src/gui/DisclaimerAction.java
+++ src/gui/DisclaimerAction.java
@@ -5,21 +5,24 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class DisclaimerAction implements ActionListener {
+    /**
+     * @wbp.parser.entryPoint
+     */
     public void actionPerformed(ActionEvent e) {
         JFrame helpScreen = new JFrame();
         JPanel panel = new JPanel();
-        ImageIcon imageIcon = new ImageIcon("/Users/victorspolaor/Downloads/unicampLogo.png");
+        ImageIcon imageIcon = new ImageIcon("unicampLogo.png");
         JLabel imageLabel = new JLabel(imageIcon);
         JTextArea helpTextArea = new JTextArea(10, 25);
         JScrollPane scrollPane = new JScrollPane(helpTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
         // Help JFrame configurations
-        helpScreen.setTitle("Disclaimer for Unit Converter - Ver. 1.0 - November, 2020");
+        helpScreen.setTitle("Disclaimer for Traffic Lights - Ver. 1.0 - December, 2020");
         helpScreen.setVisible(true);
         helpScreen.setSize(600, 200);
 
         // Inserting help JTextArea
-        String helpText = "This program was written as a part of a college project, it's intention is to demonstrate resources of Graphical User Interface (Swing) through the conversion between to measure units.\n\n Menu options:\n * In the 'Converter' menu is where the conversion operations are set\n * In the 'Help' menu informations about the program and how to use it are displayed\n\n Hope that's useful to you!";
+        String helpText = "This program was written as a part of a college project, it's intention is to demonstrate resources of Graphical User Interface (Swing) through a distribued program create in order to demonstrate the control of a semaphore simulated network";
         helpTextArea.setLineWrap(true);
         helpTextArea.setWrapStyleWord(true);
         helpTextArea.append(helpText);
@@ -28,6 +31,6 @@ public class DisclaimerAction implements ActionListener {
         // Inserting components into JFrame
         panel.add(imageLabel);
         panel.add(scrollPane, BorderLayout.PAGE_START);
-        helpScreen.add(panel);
+        helpScreen.getContentPane().add(panel);
     }
 }
--- src/gui/HelpAction.java
+++ src/gui/HelpAction.java
@@ -5,21 +5,24 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class HelpAction implements ActionListener{
+    /**
+     * @wbp.parser.entryPoint
+     */
     public void actionPerformed(ActionEvent e){
         JFrame howToScreen = new JFrame();
         JPanel panel = new JPanel();
-        ImageIcon imageIcon = new ImageIcon("/Users/victorspolaor/Downloads/unicampLogo.png");
+        ImageIcon imageIcon = new ImageIcon("unicampLogo.png");
         JLabel imageLabel = new JLabel(imageIcon);
         JTextArea howToTextArea = new JTextArea(10, 25);
         JScrollPane scrollPane = new JScrollPane(howToTextArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
         // Help JFrame configurations
-        howToScreen.setTitle("How to use it - Ver. 1.0 - November, 2020");
+        howToScreen.setTitle("How to use it - Ver. 1.0 - December, 2020");
         howToScreen.setVisible(true);
         howToScreen.setSize(600, 200);
 
         // Inserting how to JTextArea
-        String helpText = "In terms of using the software, it's very easy and intuitive:\n * In the combox right below the menu, choose the type of conversion you want to make\n * Insert the numeric value you want to convert in front of the 'Convert from' label\n * Choose the units you want to convert from and convert to\n * Click on the 'convert' button to make the conversion";
+        String helpText = "In terms of using the software, it's very easy and intuitive:\n * In order to use it you just need to execute the TrafficLightServer class, this class represents the application server-side, which is responsible for monitoring the semaphores\n * Then you execute the TrafficLight class, this class represents the application client-side, which is responsible for executing the semaphores, each time this class is executed, a new semaphore is created and the server register it while the transactions continue\n ** To end a semaphore execution you can just close the client window where it's inserted\n ** To end the the program execution, you can just close the server window";
         howToTextArea.setLineWrap(true);
         howToTextArea.setWrapStyleWord(true);
         howToTextArea.append(helpText);
@@ -28,6 +31,6 @@ public class HelpAction implements ActionListener{
         // Inserting components into JFrame
         panel.add(imageLabel);
         panel.add(scrollPane, BorderLayout.PAGE_START);
-        howToScreen.add(panel);
+        howToScreen.getContentPane().add(panel);
     }
 }
